9504244183197a016fcf37e8e56d28c485faf1f1
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-12 19:50:10 +0000

    Object task

--- day-1/04-Stu-Object-Methods/starter/script.js
+++ day-1/04-Stu-Object-Methods/starter/script.js
@@ -1,1 +1,18 @@
 //WRITE YOUR CODE BELOW
+
+var menu = {
+  coffee: 2,
+  water: 3,
+  beer: 4,
+};
+
+var prices = Object.values(menu);
+
+var total = 0;
+
+console.log("Menu list: ", Object.keys(menu));
+
+for (var i = 0; i < prices.length; i++) {
+  total += prices[i];
+}
+console.log("Total menu cost:  " + total);
