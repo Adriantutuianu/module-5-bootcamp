639c35a9e946e6a5dcae0f382a0375bbf7a5c650
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-12 19:00:10 +0000

    Day 1 task 1 Objects

--- day-1/02-Stu-Objects/starter/script.js
+++ day-1/02-Stu-Objects/starter/script.js
@@ -1,1 +1,15 @@
 //WRITE YOUR CODE BELOW
+var customerObject = {
+  drinkName: "capucinno",
+  numberSugars: 2,
+  drinkReady: true,
+};
+
+console.log("Drink name: " + customerObject.drinkName);
+console.log("Number of sugars: " + customerObject.numberSugars);
+
+if (customerObject.drinkReady) {
+  console.log("Ready for pick-up");
+} else {
+  console.log("Still in order queue");
+}
