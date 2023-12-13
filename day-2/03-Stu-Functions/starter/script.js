1608d4c512e7742b4d152929215c3e787669c788
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-13 19:18:27 +0000

    Functions task 1

--- day-2/03-Stu-Functions/starter/script.js
+++ day-2/03-Stu-Functions/starter/script.js
@@ -1,1 +1,28 @@
 // Write Your Code Below
+// * One function that adds 1 and 2 together using Function Declaration.
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(2, 1);
+
+// * One function that subtracs 10 from 20 using Function Expression.
+
+var substract = function (a, b) {
+  console.log(a - b);
+};
+
+substract(20, 10);
+
+// * One function that calculates the area of a square using an outside variable, using either function declaration or expression.
+
+// Function declaration
+function calculateSquareArea(sideLength) {
+  // Assuming 'outsideVariable' is the outside variable
+  return sideLength * sideLength * outsideVariable;
+}
+
+// Example usage
+var outsideVariable = 2; // Set the outside variable
+var area = calculateSquareArea(5); // Calculate the area with a side length of 5
+console.log(area); // Output: 50
